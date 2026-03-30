bc8fbb30c11bb381819aff13366de60c24c3fe21
Mihad <abopobackermihad@gmail.com>
2026-03-31 04:08:53 +0530

    Modified Calculator

---
+++ Calculator/calculator.py
@@ -0,0 +1,57 @@
+
+def additional(num1,num2):
+    additional=num1+num2
+    return additional
+
+def subtraction(num1,num2):
+    subtraction=num1-num2
+    return subtraction
+
+def  multipication(num1,num2):
+    multipication=num1*num2
+    return multipication
+
+def division(num1,num2):
+    division=num1/num2
+    return division
+
+def modules(num1,num2):
+    modules=num1%num2
+    return modules
+
+
+while True :
+    print("1: additional")
+    print("2: subtraction")
+    print("3: multipication")
+    print("4: division")
+    print("5: modules")
+    print("6: exit")
+    options = input("Select your option (1/2/3/4/5/6) :")
+
+    if options == '6':
+        print("exited")
+        break
+
+    if options in ('1','2','3','4','5'):
+        num1 = int(input("Enter a First Number :"))
+        num2 = int(input("Enter a Second Number :"))
+
+        if options == '1':
+            print("Result :", additional(num1, num2))
+        elif options == '2':
+            print("Result :", subtraction(num1, num2))
+        elif options == '3':
+            print("Result :", multipication(num1, num2))
+        elif options == '4':
+            print("Result :", division(num1, num2))
+        elif options == '5':
+            print("Result :", modules(num1, num2))
+        else:
+            print("Invalid Options")
+
+
+
+
+
+
